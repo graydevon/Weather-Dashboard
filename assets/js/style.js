6a60d8a7f1f53ca687349e37e48926892e00d350
Devon <devondgray28@gmail.com>
2022-06-12 23:10:46 -0500

    searched cities

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,7 +1,20 @@
-var APIKey = "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";
+var APIKey = "62df97dad542aecffdabda6ba9e1ae5f";
 var listBoxEl = document.getElementById("cityList");
 var cityInputEl = document.getElementById("citySearch")
 var searchButtonEl = document.getElementById("searchButton")
 var displayBoxEl = document.getElementById("display");
 var forecastBoxEl = document.getElementById("forecast");
 var city;
+
+//to get location of city
+var getLocation = function (city) {
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
+
+//to get previous searched cities
+var retrieveCities = function () {
+    var retrievedCity = localStorage.getItem("newCty");
+    console.log(retrievedCity);
+    if(retrievedCity){
+    addCity(retrievedCity);
+    }
+  };
