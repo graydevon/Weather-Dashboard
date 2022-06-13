cbfc0d02620f627c8515129649d9f3b386193cd8
Devon <devondgray28@gmail.com>
2022-06-12 22:56:35 -0500

    variables and api key

---
+++ assets/js/style.js
@@ -0,0 +1,7 @@
+var APIKey = "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";
+var listBoxEl = document.getElementById("cityList");
+var cityInputEl = document.getElementById("citySearch")
+var searchButtonEl = document.getElementById("searchButton")
+var displayBoxEl = document.getElementById("display");
+var forecastBoxEl = document.getElementById("forecast");
+var city;
